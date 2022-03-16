d2f1fdd8a8e9021dab8fafdf28a3a3e454db2401
qikun <qikun1990@gmail.com>
2022-03-16 14:06:30 +0800

    update

--- src/main/java/com/tangtang/po/UserBaseInfo.java
+++ src/main/java/com/tangtang/po/UserBaseInfo.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
@@ -16,6 +18,8 @@ public class UserBaseInfo implements Serializable, Cloneable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private Integer id;
 	
 	@Column(name = "subid")
